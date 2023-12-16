e3ad78531689b8cea1e244d7aaa5d27a222344cc
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2023-12-16 16:24:00 +0200

    done step 2

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 export class ContactForm extends Component {
   state = {
     name: '',
+    number: '',
   };
 
   handleSubmitForm = e => {
@@ -41,6 +42,18 @@ export class ContactForm extends Component {
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               value={this.state.name}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label className={css.phonebookFormItem}>
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={this.state.number}
               onChange={this.handleInputChange}
             />
           </label>
